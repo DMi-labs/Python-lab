5a6cb33f954fffb20f9a37c44b362ad8d7db395f
Sangwani-J0E <mkandawiresangwanijoe@gmail.com>
2022-03-24 21:09:37 +0200

    Multiplication table by SangwaniMKT uploded

---
+++ maltipliccation table by Sangwani J0E.py
@@ -0,0 +1,22 @@
+#table for x1 to x6
+num = 1, 2, 3, 4, 5, 6
+for i in range(1, 13):
+   print(1, 'x', i, '=', 1*i, "\t",\
+    2, 'x', i, '=', 2*i, "\t",\
+    3, 'x', i, '=', 3*i, "\t",\
+    4, 'x', i, '=', 4*i, "\t",\
+    5, 'x', i, '=', 5*i, "\t",\
+    6, 'x', i, '=', 6*i)
+print('')
+#table for x7 to x12
+num = 7, 8, 9, 10, 12
+for i in range(1, 13):
+
+   print(7, 'x', i, '=', 7*i, "\t",\
+    8, 'x', i, '=', 8*i, "\t",\
+    9, 'x', i, '=', 9*i, "\t",\
+    10, 'x', i, '=', 10*i, "\t",\
+    11, 'x', i, '=', 11*i, "\t",\
+    12, 'x', i, '=', 12*i, "\t")
+print()
+print(" MULTIPLICATION       TABLE        PY        CODE         EXECUTED           SUCCESSFULLY")
