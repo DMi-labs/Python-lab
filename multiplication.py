abd5f8c8d125dc2d9ba50a798b55791e460b1dae
OmarComish <comish.omar@gmail.com>
2022-03-24 16:34:10 +0200

    Multiplication quiz uploaded

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+print ('hello world!')
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+dict = {'firstname':'comish','lastname':'omar','age':55,
+          'designation': 'Marketing Manager','nums':[100,300,500,1000,2,389,40,7,3,5,12,24,50]}
+
+print(dict['nums'])
