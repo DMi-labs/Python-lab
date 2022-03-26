b10260907c7b6d424416537a83979305fc44af56
cedricngendahimana <cypscedricj@gail.com>
2022-03-26 06:21:33 -0700

    cedric Multiplication table Succesfully uploaded

---
+++ TIMES TABLE.py
@@ -0,0 +1,23 @@
+print("\t\t\t\t\t\t\tmultiplication times table chart\n")
+num=1
+num2=2
+num3=3
+num4=4
+num5=5
+num6=6
+num7=7
+num8=8
+num9=9
+num10=10
+num11=11
+num12=12
+#table 1
+for i in range (0,13):
+    print(num,"x",i,"=",1*i,"\t\t",num2,"x", i,"=",2*i," \t\t",num3,"x",i, "=",3*i,"\t\t",num4,"x", i, "=",4*i,"\t\t",num5, "x", i, "=", 5*i,"\t\t ",num6, "x", i, "=", 6*i,"\t\t",)  
+
+#table 2
+print("\n")
+for i in range (0,13):
+    print(num7, "x",i, "=",7*i,"\t\t",num8, "x", i, "=",8*i, " \t\t",num9, "x", i, "=", 9*i,"\t\t",num10, "x", i, "=", 10*i,"\t\t",num11, "x", i, "=", 11*i,"\t\t ",num12, "x", i, "=", 12*i,"\t\t",)  
+    
+   
