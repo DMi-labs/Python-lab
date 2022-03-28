cf3d44c019bc1dd8ec99189e6cd913967906a266
Innocentkazembe <102282242+Innocentkazembe@users.noreply.github.com>
2022-03-28 02:49:23 -0700

    Update MultiplicationTable.py

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -1,74 +1,59 @@
-k=1
-k = int(input("Enter the number:"))
+num= int(input("Enter the num:"))
 print("the multiplication table of:", 1)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=2
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(1,'x',count,'=',1*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 2)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=3        
-k= int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(2,'x',count,'=',2*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 3)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=4
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(3,'x',count,'=',3*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 4)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=5
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(4,'x',count,'=',4*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 5)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=6
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(5,'x',count,'=',5*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 6)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k =7       
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(6,'x',count,'=',6*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 7)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=8        
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(7,'x',count,'=',7*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 8)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=9        
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(8,'x',count,'=',8*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 9)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-
-k=10
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(9,'x',count,'=',9*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 10)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=11        
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(10,'x',count,'=',10*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 11)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-        print()
-k=12                
-k = int(input("Enter the number:"))
-print("the multiplication table of:", k)
+        print(11,'x',count,'=',11*count)
+print()
+num= int(input("Enter the num:"))
+print("the multiplication table of:", 12)
 for count in range(1, 13):
-        print(k,'x',count,'=',k*count)
-                        
-print(Good bye!!!)                
-        
+        print(12,'x',count,'=',12*count)
